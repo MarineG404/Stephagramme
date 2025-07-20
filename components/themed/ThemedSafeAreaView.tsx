80baaa00247839b6353dbf7e16dbab6d8205a156
SkyWors <skyworslegrand@gmail.com>
2025-07-20 23:00:26 +0200

    Oui

--- components/themed/ThemedSafeAreaView.tsx
+++ components/themed/ThemedSafeAreaView.tsx
@@ -1,33 +1,15 @@
+import { SafeAreaView } from 'react-native-safe-area-context';
+
 import { useThemeColor } from '@/hooks/useThemeColor';
-import React from 'react';
-import type { StyleProp, ViewProps, ViewStyle } from 'react-native';
-import { SafeAreaView, useSafeAreaInsets } from 'react-native-safe-area-context';
+import { ViewProps } from 'react-native';
 
 export type ThemedSafeAreaViewProps = ViewProps & {
-	lightColor?: string;
-	darkColor?: string;
-	style?: StyleProp<ViewStyle>;
+    lightColor?: string;
+    darkColor?: string;
 };
 
 export function ThemedSafeAreaView({ style, lightColor, darkColor, ...otherProps }: ThemedSafeAreaViewProps) {
-	const insets = useSafeAreaInsets();
+    const backgroundColor = useThemeColor({ light: lightColor, dark: darkColor }, 'background');
 
-	const backgroundColor = useThemeColor({ light: lightColor ?? '#222a22', dark: darkColor ?? '#222a22' }, 'background');
-
-	return (
-		<SafeAreaView
-			style={[
-				{
-					backgroundColor,
-					paddingTop: insets.top + 8,
-					paddingBottom: insets.bottom,
-					paddingLeft: insets.left,
-					paddingRight: insets.right,
-					flex: 1,
-				},
-				style,
-			]}
-			{...otherProps}
-		/>
-	);
+    return <SafeAreaView style={[{ backgroundColor }, style]} {...otherProps} />;
 }
